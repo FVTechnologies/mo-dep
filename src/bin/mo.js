e4d9b68c56697ec234a957da3092b0c8e6b21b2d
Alfi Salvacion <alfi@flightvector.com>
2024-10-17 23:09:52 +0800

    [1] Resolve errors found on the main app by updating dependent code

--- src/bin/mo.js
+++ src/bin/mo.js
@@ -1,40 +1,42 @@
 #!/usr/bin/env node
 
-import program from 'commander';
+import { Command } from 'commander';
 import generate from '../generators';
 import destroy from '../destroyers';
 import serve from '../server/serve';
-import {version} from '../../package.json';
+import { version } from '../../package.json';
+
+const program = new Command();
 
 program
-    .command('new [name]')
-    .description('create new app')
-    .action((name) => generate('app', {name}));
+  .command('new [name]')
+  .description('create new app')
+  .action((name) => generate('app', { name }));
 
 program
-    .command('generate [target] [name]')
-    .alias('g')
-    .option('-f, --force', 'overwrite existing folders')
-    .description('run specific generator (ex. `mo g page Home`)')
-    .action((target, name, options={}) => generate(target, {name, options}));
+  .command('generate [target] [name]')
+  .alias('g')
+  .option('-f, --force', 'overwrite existing folders')
+  .description('run specific generator (ex. `mo g page Home`)')
+  .action((target, name, options) => generate(target, { name, options }));
 
 program
-    .command('destroy [target] [name]')
-    .description('destroy specific targe (ex. `mo destroy page Home`)')
-    .action((target, name, options={}) => destroy(target, {name, options}));
+  .command('destroy [target] [name]')
+  .description('destroy specific target (ex. `mo destroy page Home`)')
+  .action((target, name, options) => destroy(target, { name, options }));
 
 program
-    .command('serve')
-    .alias('s')
-    .option('-p, --port [port]', 'express.js port (defaults to 7777)')
-    .allowUnknownOption()
-    .description('start dev server (ex. `mo s`)')
-    .action((options) => serve({
-        source: process.cwd(),
-        port: options.port
-    }));
+  .command('serve')
+  .alias('s')
+  .option('-p, --port <port>', 'express.js port (defaults to 7777)')
+  .allowUnknownOption()
+  .description('start dev server (ex. `mo s`)')
+  .action((options) => serve({
+    source: process.cwd(),
+    port: options.port || 7777
+  }));
 
 program
-    .description('Run `mo [command] -h` to show command options')
-    .version(version)
-    .parse(process.argv);
+  .description('Run `mo [command] -h` to show command options')
+  .version(version)
+  .parse(process.argv);
--- src/server/serve.js
+++ src/server/serve.js
@@ -2,7 +2,6 @@ import path from 'path';
 import express from 'express';
 import yargs from 'yargs';
 import webpack from 'webpack';
-import parseConfig from 'webpack/bin/convert-argv';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import ip from 'ip';
@@ -10,28 +9,40 @@ import ip from 'ip';
 const STAGING_PATH = path.resolve(__dirname, '../../staging/');
 const DEFAULT_PORT = 7777;
 
-export default function ({source, port = DEFAULT_PORT}) {
-    const webpackConfig = parseConfig(yargs, {
-        env: {dev: true},
-        ...yargs.argv,
-        _: '',
-        $0: ''
-    });
-    const publicPath = webpackConfig.output.publicPath;
-    const publicPathAbsolute = path.join(source, publicPath);
-    const compiler = webpack(webpackConfig);
-    const localIp = ip.address();
+export default function ({ source, port = DEFAULT_PORT }) {
+  // Adjust how webpack config is parsed.
+  const argv = yargs
+    .env('dev')
+    .option('mode', {
+      alias: 'm',
+      default: 'development',
+      describe: 'Webpack mode',
+      choices: ['development', 'production'],
+    })
+    .argv;
 
-    express()
-        .use(webpackDevMiddleware(compiler, {publicPath: '/src', stats: {colors: true}}))
-        .use(webpackHotMiddleware(compiler))
-        .use(publicPath, express.static(publicPathAbsolute))
-        .use(express.static(STAGING_PATH))
-        .listen(port, (err) => {
-            if (err) {
-                console.error(err)
-            } else {
-                console.log(`\n\nYour mo 🐍  server is ready. Ctrl+click => http://${localIp}:${port} !\n\n`);
-            }
-        });
+  // Load the Webpack configuration dynamically.
+  const webpackConfig = require(path.resolve(source, 'webpack.config.js'));
+  webpackConfig.mode = argv.mode;
+
+  const compiler = webpack(webpackConfig);
+  const publicPath = webpackConfig.output.publicPath || '/';
+  const publicPathAbsolute = path.join(source, publicPath);
+  const localIp = ip.address();
+
+  express()
+    .use(webpackDevMiddleware(compiler, {
+      publicPath: webpackConfig.output.publicPath,
+      stats: { colors: true },
+    }))
+    .use(webpackHotMiddleware(compiler))
+    .use(publicPath, express.static(publicPathAbsolute))
+    .use(express.static(STAGING_PATH))
+    .listen(port, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`\n\nYour mo 🐍  server is ready. Ctrl+click => http://${localIp}:${port} !\n\n`);
+      }
+    });
 }
