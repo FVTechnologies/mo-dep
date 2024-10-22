1eba1634e6cd4cea6b554f36c65cac2d5ee24062
Alfi Salvacion <alfi@flightvector.com>
2024-10-22 23:28:54 +0800

    Attempt to fix issue

--- templates/app/webpack.config.js
+++ templates/app/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 const HtmlIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
